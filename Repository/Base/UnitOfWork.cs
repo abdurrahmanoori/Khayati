9eb26f31cef7a8a29f5feac09cd09f0eae170f5a
Khubaib Ghufran <khubaibghufran415@gmail.com>
2024-09-02 19:31:46 +0430

    Generic and Unit of Work Classes and Interfaces added.

---
+++ Repository/Base/UnitOfWork.cs
@@ -0,0 +1,31 @@
+﻿using Entities.Data;
+using RepositoryContracts.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories.Base
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ApplicationDbContext _db;
+        //private readonly ISurveyRepository SurveyRepository;
+        //private IPatientTypeRepository _patientTypeRepository;
+
+        public UnitOfWork(ApplicationDbContext db)
+        {
+            _db = db;
+           // SurveyRepository = new SurveyRepository(db);
+            // ProvinceRepository = new ProvinceRepository(db);
+        }
+
+       // public ISurveyRepository SurveyRepository { get; private set; }
+
+        public async Task SaveChanges(CancellationToken cancellationToken)
+        {
+            await _db.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
