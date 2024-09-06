b139bf5ce01831ec96b78853b9c4c106fa969222
Abdurrahman <Abdurrahmannoori10@gmail.com>
2024-09-06 05:54:56 +0430

    embilishtype and emilish added

---
+++ Entities/Embellish.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public  class Embellish
+    {
+        public int EmbellishId { get; set; }
+
+        public string EmbelishName { get; set; }
+
+        public int? EmbellishTypeId { get; set; }
+
+
+        public virtual EmbellishType? EmbellishType { get; set; }
+
+        //public virtual ICollection<OrderEmbellish>? OrderEmbellishes { get; set; }
+    }
+}
---
+++ Entities/EmbellishType.cs
@@ -0,0 +1,13 @@
+﻿namespace Entities
+{
+    public class EmbellishType
+    {
+        public int  EmbelishTypeId { get; set; }
+
+        public string EmbelishTypeName { get;set; }
+
+
+        public virtual ICollection<Embellish>? Embellishes { get; set; }
+
+    }
+}
