c5b688ed278566ab885a0398dcae0a5f62299250
Abdurrahman <Abdurrahmannoori10@gmail.com>
2024-09-06 12:52:03 +0430

    commit after pull

---
+++ Repository/EmbellishRepository.cs
@@ -0,0 +1,24 @@
+﻿using Entities;
+using Entities.Data;
+using Repositories.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories
+{
+    public class EmbellishRepository:GenericRepository<Embellish>
+    {
+        private readonly ApplicationDbContext _dbcontext;
+
+        public EmbellishRepository(ApplicationDbContext dbContext) : base(dbContext)  
+            
+        {
+            _dbcontext = dbContext;
+        }
+
+
+    }
+}
