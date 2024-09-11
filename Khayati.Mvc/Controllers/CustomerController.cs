527f03e97938a0a53b3f14ee0c0cd2ff20447d55
Abdurrahman <Abdurrahmannoori10@gmail.com>
2024-09-11 23:14:40 +0430

    customer controller and  its simple view created.

---
+++ Khayati.Mvc/Controllers/CustomerController.cs
@@ -0,0 +1,21 @@
+﻿using Khayati.ServiceContracts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Khayati.Mvc.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private readonly ICustomerService _customerService;
+
+        public CustomerController(ICustomerService customerService)
+        {
+            _customerService = customerService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var result = await _customerService.GetCustomerList();
+            return View(result);
+        }
+    }
+}
