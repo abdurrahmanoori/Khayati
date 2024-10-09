ec9fe551a66b50d992c37307b039540ecc609b2a
Abdurrahman <Abdurrahmannoori10@gmail.com>
2024-10-09 07:39:49 +0430

    tranlation entity added

---
+++ Khayati.Core/Domain/Entities/Translation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Khayati.Core.Domain.Entities
+{
+    public class Translation
+    {
+        public int TranslationId { get; set; }
+        public string EntityType { get; set; }  // e.g., "Province", "District"
+        public int EntityId { get; set; }       // ID of the entity being translated (ProvinceId, DistrictId)
+        public string LanguageCode { get; set; }  // Language code like "en", "fr"
+        public string PropertyName { get; set; }  // e.g., "ProvinceName"
+        public string TranslatedValue { get; set; }  // Translated text
+    }
+}
