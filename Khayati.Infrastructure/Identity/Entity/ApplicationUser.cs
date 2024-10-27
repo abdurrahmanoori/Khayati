fdbf2b7272a3c70fd2237e1a40ca42c16e181e78
Abdurrahman <Abdurrahmannoori10@gmail.com>
2024-10-27 07:32:43 +0430

    only identity user entity added.

---
+++ Khayati.Infrastructure/Identity/Entity/ApplicationUser.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Khayati.Infrastructure.Identity.Entity
+{
+    public class ApplicationUser
+    {
+    }
+}
