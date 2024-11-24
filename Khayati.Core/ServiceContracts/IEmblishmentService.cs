23023133c0558214a61d02f9dfad8e83cbee5bc3
Abdurrahmanoori <Abdurrahmannoori10@gmail.com>
2024-11-24 06:13:12 +0430

    embellishment update operatio done.

--- Khayati.Core/ServiceContracts/IEmblishmentService.cs
+++ Khayati.Core/ServiceContracts/IEmblishmentService.cs
@@ -9,10 +9,11 @@ namespace Khayati.ServiceContracts
         public Task<EmbellishmentAddDto> AddEmbellishment(EmbellishmentAddDto addEmbellishmentDto);
         public Task<EmbellishmentResponseDto> GetEmbellishmentById(int? EmbellishmentId);
         public Task<EmbellishmentDetailDto> GetEmbellishmentDetails(int? EmbellishmentId);
-
         public Task<Result<IEnumerable<EmellishmentResponseDetailsDto>>> GetEmbellishmentList();
 
         public Task<EmbellishmentResponseDto> DeleteEmbellishment(int? EmbellishmentId);
+
+        public Task<Result<bool>> Update(int embellishmentId, EmbellishmentUpdateDto updateDto);
         // public Task<EmbellishmentResponseDto> UpdateEmbellishment(int? EmbellishmentId);
 
     }
