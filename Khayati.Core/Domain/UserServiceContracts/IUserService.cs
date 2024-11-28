6d35bca7d5b95ed85ca51c4c9d7e858e18334bb3
= <=>
2024-11-28 12:35:08 +0430

    some name refactored.

--- Khayati.Core/Domain/UserServiceContracts/IUserService.cs
+++ Khayati.Core/Domain/UserServiceContracts/IUserService.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Khayati.Core.Domain.UserServiceContracts
+﻿namespace Khayati.Core.Domain.UserServiceContracts
 {
     public interface IUserService
     {
