a364f4157e30d4f650e478b218cd001c283bf96d
= <=>
2025-02-15 13:40:18 +0430

    custmoer drop down lsit added.

---
+++ Khayati.Mvc/Areas/Admin/Controllers/Api/CustomerController.cs
@@ -0,0 +1,32 @@
+﻿using Khayati.ServiceContracts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Khayati.Mvc.Areas.Admin.Controllers.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        private readonly ICustomerService customerService;
+        public CustomerController(ICustomerService customerService)
+        {
+            this.customerService = customerService;
+        }
+
+
+        [HttpGet("GetCustomers")]
+        public async Task<IActionResult> GetCustomers( )
+        {
+            var customers = await customerService.GetCustomerList();
+            return Ok(
+                customers.Select(x => new
+                {
+                    Id = x.CustomerId,
+                    Name = x.Name
+                })
+            );
+
+
+        }
+    }
+}
