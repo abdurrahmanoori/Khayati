265e93ba3e0feb1a1e9d2d3148caaae074bc2857
= <=>
2025-02-18 15:37:53 +0430

    Add getPriorities endpoint to GeneralController
    Implemented a new HTTP GET endpoint `getPriorities` that retrieves all values of the `OrderPriority` enum, converts them to a dictionary, and returns it as an HTTP response with a status of OK.

--- Khayati.Mvc/Areas/Admin/Controllers/Api/GeneralController.cs
+++ Khayati.Mvc/Areas/Admin/Controllers/Api/GeneralController.cs
@@ -31,7 +31,15 @@ namespace Khayati.Mvc.Areas.Admin.Controllers.Api
 
         }
 
-      
+        [HttpGet("getPriorities")]
+        public IActionResult GetPriorities( )
+        {
+            var priorities = Enum.GetValues(typeof(OrderPriority))
+                                 .Cast<OrderPriority>()
+                                 .ToDictionary(e => (int)e, e => e.ToString());
+
+            return Ok(priorities);
+        }
 
         [HttpGet("getPaymentStatus")]
         public IActionResult GetPaymentStatus( )
