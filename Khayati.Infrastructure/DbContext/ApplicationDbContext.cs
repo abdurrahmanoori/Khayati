b4cfe0159c77060891606a262c4c55b37b4fc788
abubakr <abubakr.kaakar.2017@gmail.com>
2025-07-20 15:42:01 +0430

    orderDesign is added

--- Khayati.Infrastructure/DbContext/ApplicationDbContext.cs
+++ Khayati.Infrastructure/DbContext/ApplicationDbContext.cs
@@ -37,12 +37,6 @@ namespace Entities.Data
                 .Property(x => x.ProductionStatus)
                 .HasConversion<string>();
 
-            modelBuilder.Entity<OrderDesign>()
-        .HasOne(od => od.Order)
-        .WithMany(o => o.OrderDesigns)
-        .HasForeignKey(od => od.OrderId)
-        .OnDelete(DeleteBehavior.Cascade);
-
             modelBuilder.Entity<Payment>()
      .HasOne(p => p.Order)
      .WithMany(o => o.Payments)
