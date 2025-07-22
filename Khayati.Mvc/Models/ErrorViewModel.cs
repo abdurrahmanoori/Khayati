6110ebd349a80b6653774020efc01f9d0c7b7a6e
ARD-Developer <abdurrahmannoori10@gmail.com>
2025-07-22 09:25:59 +0430

    mvc project removed

--- Khayati.Mvc/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Khayati.Mvc.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
