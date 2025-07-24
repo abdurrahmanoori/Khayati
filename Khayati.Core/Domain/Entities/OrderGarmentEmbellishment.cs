80279f862e39b7eddd42624198b8593582bfee53
Abdurrahman <Abdurrahmannoori10@gmail.com>
2025-07-24 05:27:54 +0430

    any async added in generic repo

--- Khayati.Core/Domain/Entities/OrderGarmentEmbellishment.cs
+++ Khayati.Core/Domain/Entities/OrderGarmentEmbellishment.cs
@@ -8,7 +8,6 @@ namespace Entities
     {
         [Key]
         public int Id { get; set; }
-        public int OrderDesignId { get; set; }
         public int EmbellishmentId { get; set; }
         /// <summary>
         /// Optional free-form notes from the designer or client (e.g., "Use gold thread"). Provides clarity for custom jobs.
@@ -25,9 +24,7 @@ namespace Entities
         /// Name or ID of the worker/vendor who applied the embellishment. Useful for tracking accountability or quality control.
         /// </summary>
         public int? AppliedBy { get; set; }
-        [ForeignKey(nameof(OrderDesignId))]
-        public OrderDesign? Orderdesign { get; set; }
-
+      
         [ForeignKey(nameof(EmbellishmentId))]
         public Embellishment? Embellishment { get; set; }
 
